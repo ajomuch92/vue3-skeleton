abea6e0c6fe9041b3f308572769f13d4e9871cde
Aarón J. Montes <aaronmontes_92@yahoo.es>
2024-10-14 19:28:57 -0600

    refactor: 💡 Change export default for index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import Vue3Skeleton from './components/Skeleton.vue';
 
-export default function install(app) {
-  app.component('skeleton', Vue3Skeleton);
-}
-
-export const Skeleton = Vue3Skeleton;
+export default Vue3Skeleton;
