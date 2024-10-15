7188633cdf4f0101c0a1aa68e0b3e3066e1cef2f
Aarón J. Montes <aaronmontes_92@yahoo.es>
2024-10-14 21:47:07 -0600

    refactor: 💡 Update configuration files to include css files

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), libInjectCss()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'Vue3Skeleton',
       fileName: 'v3-skeleton',
     },
+    copyPublicDir: false,
     cssCodeSplit: true,
     rollupOptions: {
       external: ['vue'],
